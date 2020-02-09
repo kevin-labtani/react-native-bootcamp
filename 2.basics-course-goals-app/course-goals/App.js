2489a279038fad27422364bdd0e0b80f40aa3d5e
Kevin Labtani <kevinlabtani@gmail.com>
2020-02-09 20:03:03 +0100

    usestate hooks

--- 2.basics-course-goals-app/course-goals/App.js
+++ 2.basics-course-goals-app/course-goals/App.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState("");
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const goatInputHandler = enteredText => {
+    setEnteredGoal(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    // pass in a function to make sure coursegoals are up to date
+    // rather than setCourseGoals([...courseGoals, enteredGoal]);
+    setCourseGoals(currentGoals => [...currentGoals, enteredGoal]);
+  };
+
   return (
     <View style={styles.screen}>
       <View style={styles.inputContainer}>
-        <TextInput placeholder="Course Goals" style={styles.textInput} />
-        <Button title="ADD" />
+        {/* onchanetext execute on every keystroke */}
+        <TextInput
+          placeholder="Course Goals"
+          style={styles.textInput}
+          onChangeText={goatInputHandler}
+          value={enteredGoal}
+        />
+        <Button title="ADD" onPress={addGoalHandler} />
       </View>
       <View></View>
     </View>
