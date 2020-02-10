487bb2daf19a54eba19bf7dc058df14f26f50642
Kevin Labtani <kevinlabtani@gmail.com>
2020-02-10 09:50:44 +0100

    style buttons

--- 2.basics-course-goals-app/course-goals/components/GoalInput.js
+++ 2.basics-course-goals-app/course-goals/components/GoalInput.js
@@ -23,9 +23,15 @@ const GoalInput = props => {
           onChangeText={goalInputHandler}
           value={enteredGoal}
         />
-        <Button title="CANCEL" color="red" onPress={props.onCancel} />
-        <Button title="ADD" onPress={addGoalHandler} />
-        {/* <Button title="ADD" onPress={props.onAddGoal.bind(this, enteredGoal)} /> */}
+        <View style={styles.buttonContainer}>
+          <View style={styles.button}>
+            <Button title="CANCEL" color="red" onPress={props.onCancel} />
+          </View>
+          <View style={styles.button}>
+            <Button title="ADD" onPress={addGoalHandler} />
+            {/* <Button title="ADD" onPress={props.onAddGoal.bind(this, enteredGoal)} /> */}
+          </View>
+        </View>
       </View>
     </Modal>
   );
@@ -38,11 +44,19 @@ const styles = StyleSheet.create({
     alignItems: "center"
   },
   textInput: {
-    width: 200,
+    width: "80%",
     borderColor: "black",
     borderWidth: 1,
     padding: 10,
     marginBottom: 10
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "60%"
+  },
+  button: {
+    width: "40%"
   }
 });
 
