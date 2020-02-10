c45e364f86ddf6f4ffcb93092897a62cb5bc9b37
Kevin Labtani <kevinlabtani@gmail.com>
2020-02-10 16:42:53 +0100

    hints, validation & useRef

--- 4.guess-a-number-app/project/screens/GameScreen.js
+++ 4.guess-a-number-app/project/screens/GameScreen.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import React, { useState, useRef } from "react";
+import { View, Text, StyleSheet, Button, Alert } from "react-native";
 
 import NumberContainer from "../components/NumberContainer";
 import Card from "../components/Card";
@@ -21,13 +21,49 @@ const GameScreen = props => {
     generateRandomBetween(1, 100, props.userChoice)
   );
 
+  //If a component doesn't need to be updated with new data it's always best to avoid a re-render. That's the benefit of useRef over state
+  const currentLow = useRef(1);
+  const currentHigh = useRef(100);
+
+  const nextGuessHandler = direction => {
+    if (
+      // wrong hint
+      (direction === "lower" && currentGuess < props.userChoice) ||
+      (direction === "greater" && currentGuess > props.userChoice)
+    ) {
+      Alert.alert("Don't lie!", "You know that this is wrong", [
+        {
+          text: "Sorry",
+          style: "cancel"
+        }
+      ]);
+      return;
+    }
+
+    // generate new nr
+    if (direction === "lower") {
+      currentHigh.current = currentGuess;
+    } else {
+      currentLow.current = currentGuess;
+    }
+    const nextNumber = generateRandomBetween(
+      currentLow.current,
+      currentHigh.current,
+      currentGuess
+    );
+    setCurrentGuess(nextNumber);
+  };
+
   return (
     <View style={styles.screen}>
       <Text>Opponent's Guess</Text>
       <NumberContainer>{currentGuess}</NumberContainer>
       <Card style={styles.buttonContainer}>
-        <Button title="LOWER" onPress={() => {}} />
-        <Button title="GREATER" onPress={() => {}} />
+        <Button title="LOWER" onPress={nextGuessHandler.bind(this, "lower")} />
+        <Button
+          title="GREATER"
+          onPress={nextGuessHandler.bind(this, "greater")}
+        />
       </Card>
     </View>
   );
