bf0bc9542545c458257094d605b3c00e96f5d120
Kevin Labtani <kevinlabtani@gmail.com>
2020-02-10 16:05:32 +0100

    conditionally load gamescreen

--- 4.guess-a-number-app/project/screens/GameScreen.js
+++ 4.guess-a-number-app/project/screens/GameScreen.js
@@ -1,12 +1,15 @@
 import React, { useState } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Button } from "react-native";
+
+import NumberContainer from "../components/NumberContainer";
+import Card from "../components/Card";
 
 // generate random integer guess
 const generateRandomBetween = (min, max, exclude) => {
   min = Math.ceil(min);
   max = Math.floor(max);
   const rndNum = Math.floor(Math.random() * (max - min)) + min;
-  if (rndNum === excluse) {
+  if (rndNum === exclude) {
     return generateRandomBetween(min, max, exclude);
   } else {
     return rndNum;
@@ -18,9 +21,31 @@ const GameScreen = props => {
     generateRandomBetween(1, 100, props.userChoice)
   );
 
-  return;
+  return (
+    <View style={styles.screen}>
+      <Text>Opponent's Guess</Text>
+      <NumberContainer>{currentGuess}</NumberContainer>
+      <Card style={styles.buttonContainer}>
+        <Button title="LOWER" onPress={() => {}} />
+        <Button title="GREATER" onPress={() => {}} />
+      </Card>
+    </View>
+  );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 10,
+    alignItems: "center"
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginTop: 20,
+    width: 300,
+    maxWidth: "80%"
+  }
+});
 
 export default GameScreen;
