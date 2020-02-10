5de9ee4561b17f666c4aea14db1455201ff7e70c
Kevin Labtani <kevinlabtani@gmail.com>
2020-02-10 17:13:02 +0100

    finish game logic

--- 4.guess-a-number-app/project/App.js
+++ 4.guess-a-number-app/project/App.js
@@ -10,9 +10,14 @@ export default function App() {
   // track nr of guesses
   const [guessRounds, setGuessRounds] = useState(0);
 
+  // reset the game
+  const configureNewGameHandler = () => {
+    setGuessRounds(0);
+    setUserNumber(null);
+  };
+
   const startGameHandler = selectedNumber => {
     setUserNumber(selectedNumber);
-    setGuessRounds(0);
   };
 
   const gameOverHandler = numOfRounds => {
@@ -26,7 +31,13 @@ export default function App() {
       <GameScreen userChoice={userNumber} onGameOver={gameOverHandler} />
     );
   } else if (guessRounds > 0) {
-    content = <GameOverScreen />;
+    content = (
+      <GameOverScreen
+        roundsNumber={guessRounds}
+        userNumber={userNumber}
+        onRestart={configureNewGameHandler}
+      />
+    );
   }
 
   return (
