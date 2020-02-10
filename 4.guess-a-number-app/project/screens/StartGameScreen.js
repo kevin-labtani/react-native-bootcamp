f9ce433d391a10d2f7f151964d673273954e1263
Kevin Labtani <kevinlabtani@gmail.com>
2020-02-10 12:26:15 +0100

    extract card style to its own component

--- 4.guess-a-number-app/project/screens/StartGameScreen.js
+++ 4.guess-a-number-app/project/screens/StartGameScreen.js
@@ -1,18 +1,19 @@
 import React from "react";
 import { View, StyleSheet, Text, TextInput, Button } from "react-native";
+import Card from "../components/Card";
 
 const StartGameScreen = props => {
   return (
     <View style={styles.screen}>
       <Text style={styles.title}>Start a New Game!</Text>
-      <View style={styles.inputContainer}>
+      <Card style={styles.inputContainer}>
         <Text>Select a Number</Text>
         <TextInput />
         <View style={styles.buttonContainer}>
           <Button title="Reset" onPress={() => {}} />
           <Button title="Confirm" onPress={() => {}} />
         </View>
-      </View>
+      </Card>
     </View>
   );
 };
@@ -30,19 +31,7 @@ const styles = StyleSheet.create({
   inputContainer: {
     width: 300,
     maxWidth: "80%",
-    alignItems: "center",
-
-    // shadow for ios only
-    shadowColor: "black",
-    shadowOffset: { width: 0, height: 2 },
-    shadowRadius: 6,
-    shadowOpacity: 0.26,
-    // shadow for android only
-    elevation: 5,
-
-    backgroundColor: "white",
-    padding: 20,
-    borderRadius: 10
+    alignItems: "center"
   },
   buttonContainer: {
     flexDirection: "row",
