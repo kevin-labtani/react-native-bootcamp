f8ea835884fb0f2f6415bba1d985f7d9a79e27a9
Kevin Labtani <kevinlabtani@gmail.com>
2020-02-11 10:31:55 +0100

    use custom fonts

--- 4.guess-a-number-app/project/App.js
+++ 4.guess-a-number-app/project/App.js
@@ -1,14 +1,37 @@
 import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import * as Font from "expo-font";
+import { AppLoading } from "expo";
+
 import Header from "./components/Header";
 import StartGameScreen from "./screens/StartGameScreen";
 import GameScreen from "./screens/GameScreen";
 import GameOverScreen from "./screens/GameOverScreen";
 
+// load fonts with expo, nb: it's a promise
+const fetchFonts = () => {
+  return Font.loadAsync({
+    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf")
+  });
+};
+
 export default function App() {
   const [userNumber, setUserNumber] = useState();
-  // track nr of guesses
-  const [guessRounds, setGuessRounds] = useState(0);
+  const [guessRounds, setGuessRounds] = useState(0); // track nr of guesses
+  const [dataLoaded, setDataLoaded] = useState(false);
+
+  if (!dataLoaded) {
+    // return Apploading while we wait for fonts to load
+    // startAsync must be a fuction returning a promise
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setDataLoaded(true)}
+        onError={err => console.log(err)}
+      />
+    );
+  }
 
   // reset the game
   const configureNewGameHandler = () => {
